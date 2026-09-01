f9081d34e117ecff5690ae4b6ca81f253e89dba7
nugagee <adenugaolajideadewale@gmail.com>
2026-09-01 20:00:19 +0100

    build changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -127,6 +127,7 @@ export const About = () => {
       </section>
 
       {/* Visit Information */}
+      
       <section className="py-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-12">
